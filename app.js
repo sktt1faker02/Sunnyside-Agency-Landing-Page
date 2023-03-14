4eb08c9d020b241325e6117f2d78a04e2fe2c6be
Tabilin <michael.tabilin@dxc.com>
2023-03-14 20:30:15 +0800

    update finish project

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const navIcon = document.querySelector(".nav-icon");
 const headerMenu = document.querySelector(".header-nav");
+const arrowBtn = document.querySelector(".arrow");
 
 navIcon.addEventListener("click", () => {
   headerMenu.classList.toggle("showMenu");
